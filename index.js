3cddbfc3ab215d1e7e5b1eafd1965d081ec0ccbd
cristina <cristinaxavierprodutora.gmail.com>
2021-07-02 02:43:45 -0300

    jsexercicio

---
+++ index.js
@@ -0,0 +1,65 @@
+
+let pessoa = {
+nome: "Cristina",
+idade: 51,
+amigos: ["Manuel", "Luiz", "João"]
+}
+console.log(pessoa.name); 
+pessoa.cabelo = "preto";
+delete pessoa.idade;
+console.log(pessoa);
+
+  
+let cadastro= ["funcionárioA", "funcionárioB", "funcionárioC", "funcionárioD","funcionárioE"];
+   
+
+let funcionárioA= 
+{
+   Nome:'Americo',
+   idade:65,
+   telefone:99300604,
+   amigos: ['Selma', 'Delmo', 'Aurea', 'Selmo']
+};
+
+let funcionárioB=
+{
+Nome:'Bruno',
+idade:30,
+telefone:902061991,
+amigos: ['Celio',' Melo', 'Pedro', 'Selton']
+};
+
+let funcionárioC=
+{
+   Nome:'Ciro',
+   idade:52,
+   telefone:99999225,
+   amigos:[ 'Coelho', 'Lopes', 'Vitor', 'Beth'] 
+};
+
+let funcionárioD= 
+{
+   Nome:'Delcio',
+   idade: 80,
+   telefone: 99999922,
+   amigos:[ 'Pires', 'Silvio', 'Togo', 'Severio']
+};
+
+let funcionárioE=
+{
+ Nome:'Ernesto',
+idade:90,
+telefone:98888888,
+amigos:['Brás', 'Toquinho', 'Gustavo', 'Noel']
+};
+
+
+console.log(funcionárioA[0].amigos [0]);
+console.log(funcionárioB[1].amigos [1]);
+console.log(funcionárioC[2].amigos [2]);
+console.log(funcionárioD[3].amigos [3]);
+console.log(funcionárioE[4].amigos [4]);
+
+
+
+
